52f6a64f59711f3eed1dd7de7e3ed53402ce036a
Ponmani Venkatesan <ponmani529@gmail.com>
2025-03-21 19:55:21 +0530

    Import error fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import Navbar from "./SharedComponents/Navbar/Navbar";
 import Footer from "./SharedComponents/Footer/Footer";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Button from "./SharedComponents/Button/Button";
-import FavButton from "./SharedComponents/FavButton/Favbutton";
-import Card from "./SharedComponents/Card/Card";
 import DisplayStars from "./Components/DisplayStars.jsx/DisplayStars";
 import {FavContextProvider} from './Context/FavContext/FavContext'
 import DisplayFavStars from "./Components/DisplayFavStars/DisplayFavStars";
